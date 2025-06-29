26c29a9e9a3963332f3bda4d61641893ed4656ab
Dtech2preas <lefa4082@gmail.com>
2025-06-29 23:13:08 +0200

    Update MainActivity.java

--- app/src/main/java/ru/anton2319/vpnoverssh/MainActivity.java
+++ app/src/main/java/ru/anton2319/vpnoverssh/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.VpnService;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,84 +13,55 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.cardview.widget.CardView;
-import androidx.core.content.ContextCompat;
 import androidx.core.widget.NestedScrollView;
-
-import com.airbnb.lottie.LottieAnimationView;
-import com.google.android.material.button.MaterialButton;
-import com.google.android.material.card.MaterialCardView;
-import com.google.android.material.color.DynamicColors;
-import com.google.android.material.textview.MaterialTextView;
-
 import java.util.List;
-
-import ru.anton2319.vpnoverssh.R;
-import ru.anton2319.vpnoverssh.data.model.SSHAccount;
+import ru.anton2319.vpnoverssh.data.SSHConnectionProfile;
 import ru.anton2319.vpnoverssh.data.singleton.SocksPersistent;
 import ru.anton2319.vpnoverssh.data.singleton.StatusInfo;
-import ru.anton2319.vpnoverssh.data.utils.SSHAccountAdapter;
-import ru.anton2319.vpnoverssh.data.utils.SSHAccountManager;
+import ru.anton2319.vpnoverssh.data.utils.SSHConnectionProfileAdapter;
+import ru.anton2319.vpnoverssh.data.utils.SSHConnectionProfileManager;
 import ru.anton2319.vpnoverssh.services.SocksProxyService;
 import ru.anton2319.vpnoverssh.services.SshService;
-import ru.anton2319.vpnoverssh.ui.settings.SettingsActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private List<SSHAccount> sshAccountList;
-    private SSHAccount selectedAccount;
+    List<SSHConnectionProfile> sshConnectionProfileList;
+    SSHConnectionProfile selectedProfile;
     private static final String TAG = "MainActivity";
-    private Intent vpnIntent;
-    private Intent sshIntent;
-    private EditText sniInput;
-    private MaterialTextView tvStatus;
-    private MaterialTextView tvLogs;
-    private NestedScrollView scrollView;
-    private MaterialButton connectButton;
-    private LottieAnimationView animationView;
-    private MaterialCardView accountCard;
-    private MaterialCardView sniCard;
+    private String privateKey;
+    Intent vpnIntent;
+    Intent sshIntent;
+    EditText sniInput;
+    TextView tvStatus;
+    TextView tvLogs;
+    NestedScrollView scrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        DynamicColors.applyToActivitiesIfAvailable(getApplication());
-        setContentView(R.layout.activity_main);
-        
-        if (StatusInfo.getInstance().getVpnIntent() == null) {
+        if(StatusInfo.getInstance().getVpnIntent() == null) {
             StatusInfo.getInstance().setVpnIntent(new Intent(this, SocksProxyService.class));
         }
-        if (StatusInfo.getInstance().getSshIntent() == null) {
+        if(StatusInfo.getInstance().getSshIntent() == null) {
             StatusInfo.getInstance().setSshIntent(new Intent(this, SshService.class));
         }
 
-        vpnIntent = StatusInfo.getInstance().getVpnIntent();
-        sshIntent = StatusInfo.getInstance().getSshIntent();
+        setContentView(R.layout.activity_main);
 
         // Initialize UI components
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        
         sniInput = findViewById(R.id.sni_input);
         tvStatus = findViewById(R.id.tvStatus);
         tvLogs = findViewById(R.id.tvLogs);
         scrollView = findViewById(R.id.scrollView);
-        connectButton = findViewById(R.id.ssh_connect_button);
-        animationView = findViewById(R.id.animation_view);
-        accountCard = findViewById(R.id.account_card);
-        sniCard = findViewById(R.id.sni_card);
 
-        // Set up animation
-        animationView.setAnimation(R.raw.disconnected);
-        animationView.loop(false);
-        
-        // Load accounts
-        SSHAccountManager accountManager = new SSHAccountManager(this);
-        sshAccountList = accountManager.loadAccounts();
-        SSHAccountAdapter adapter = new SSHAccountAdapter(this, sshAccountList);
+        vpnIntent = StatusInfo.getInstance().getVpnIntent();
+        sshIntent = StatusInfo.getInstance().getSshIntent();
+
+        Context context = this;
+
+        SSHConnectionProfileManager sshConnectionProfileManager = new SSHConnectionProfileManager(this);
+        sshConnectionProfileList = sshConnectionProfileManager.loadProfiles();
+        SSHConnectionProfileAdapter adapter = new SSHConnectionProfileAdapter(this, sshConnectionProfileList);
 
         Spinner spinner = findViewById(R.id.spinner);
         spinner.setAdapter(adapter);
@@ -99,80 +69,66 @@ public class MainActivity extends AppCompatActivity {
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                selectedAccount = (SSHAccount) parent.getItemAtPosition(position);
-                appendLog("Selected: " + selectedAccount.getNickname());
-                updateAccountCard();
+                selectedProfile = SSHConnectionProfile.fromLinkedTreeMap(parent.getItemAtPosition(position));
+                appendLog("Selected profile: " + selectedProfile.getName());
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                selectedAccount = null;
-                accountCard.setVisibility(View.GONE);
             }
         });
 
         ImageButton editButton = findViewById(R.id.editProfileButton);
-        editButton.setOnClickListener(v -> {
-            if (selectedAccount != null && selectedAccount.getId() != null) {
-                Intent intent = new Intent(this, AccountEditorActivity.class);
-                intent.putExtra("account_id", selectedAccount.getId());
-                startActivity(intent);
-                appendLog("Editing: " + selectedAccount.getNickname());
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(selectedProfile != null && selectedProfile.uuid != null) {
+                    Intent intent = new Intent(context, NewConnectionActivity.class);
+                    intent.putExtra("uuid", selectedProfile.uuid.toString());
+                    startActivity(intent);
+                    appendLog("Editing profile: " + selectedProfile.getName());
+                }
             }
         });
 
         ImageButton addButton = findViewById(R.id.addProfileButton);
-        addButton.setOnClickListener(v -> {
-            Intent intent = new Intent(this, AccountEditorActivity.class);
-            startActivity(intent);
-            appendLog("Creating new account");
-        });
-
-        connectButton.setOnClickListener(view -> {
-            if (selectedAccount != null) {
-                String sniHost = sniInput.getText().toString().trim();
-                if (!sniHost.isEmpty()) {
-                    selectedAccount.setSniHost(sniHost);
-                }
-                
-                if (StatusInfo.getInstance().isActive()) {
-                    disconnect();
-                } else {
-                    connect();
-                }
-            } else {
-                Toast.makeText(this, "Please select an account", Toast.LENGTH_SHORT).show();
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, NewConnectionActivity.class);
+                startActivity(intent);
+                appendLog("Creating new profile");
             }
         });
 
-        updateConnectionState();
-    }
+        Button connectButton = findViewById(R.id.ssh_connect_button);
+        connectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(selectedProfile != null) {
+                    String username = selectedProfile.getUsername();
+                    String password = selectedProfile.getPassword();
+                    privateKey = selectedProfile.getPrivateKey();
+                    String hostname = selectedProfile.getServerIP();
+                    int port = selectedProfile.getServerPort();
+                    String sniHost = sniInput.getText().toString().trim();
 
-    private void updateAccountCard() {
-        if (selectedAccount != null) {
-            accountCard.setVisibility(View.VISIBLE);
-            TextView accountName = findViewById(R.id.account_name);
-            TextView accountDetails = findViewById(R.id.account_details);
-            
-            accountName.setText(selectedAccount.getNickname());
-            String details = String.format("%s@%s:%d", 
-                selectedAccount.getUsername(),
-                selectedAccount.getServer(),
-                selectedAccount.getPort());
-            accountDetails.setText(details);
-            
-            if (selectedAccount.getSniHost() != null && !selectedAccount.getSniHost().isEmpty()) {
-                sniInput.setText(selectedAccount.getSniHost());
-                sniCard.setVisibility(View.VISIBLE);
-            } else {
-                sniCard.setVisibility(View.GONE);
+                    appendLog("Starting connection to: " + hostname);
+                    startVpn(username, password, privateKey, hostname, port, sniHost);
+                }
+                else {
+                    appendLog("No profile selected! Creating new one");
+                    Intent intent = new Intent(context, NewConnectionActivity.class);
+                    startActivity(intent);
+                }
             }
-        }
+        });
     }
 
     private void appendLog(String message) {
         runOnUiThread(() -> {
             tvLogs.append(">> " + message + "\n");
+            // Auto-scroll to bottom
             scrollView.post(() -> scrollView.fullScroll(View.FOCUS_DOWN));
         });
     }
@@ -180,30 +136,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onResume() {
         super.onResume();
-        SSHAccountManager accountManager = new SSHAccountManager(this);
-        sshAccountList = accountManager.loadAccounts();
-        SSHAccountAdapter adapter = new SSHAccountAdapter(this, sshAccountList);
+        SSHConnectionProfileManager sshConnectionProfileManager = new SSHConnectionProfileManager(this);
+        sshConnectionProfileList = sshConnectionProfileManager.loadProfiles();
+        SSHConnectionProfileAdapter adapter = new SSHConnectionProfileAdapter(this, sshConnectionProfileList);
         Spinner spinner = findViewById(R.id.spinner);
         spinner.setAdapter(adapter);
-        
-        updateConnectionState();
-    }
 
-    private void updateConnectionState() {
         if (StatusInfo.getInstance().isActive()) {
-            connectButton.setText("DISCONNECT");
-            connectButton.setIcon(ContextCompat.getDrawable(this, R.drawable.ic_disconnect));
             tvStatus.setText("Status: Connected");
-            tvStatus.setTextColor(ContextCompat.getColor(this, R.color.connected_green));
-            animationView.setAnimation(R.raw.connected);
-            animationView.playAnimation();
         } else {
-            connectButton.setText("CONNECT");
-            connectButton.setIcon(ContextCompat.getDrawable(this, R.drawable.ic_connect));
             tvStatus.setText("Status: Disconnected");
-            tvStatus.setTextColor(ContextCompat.getColor(this, R.color.disconnected_red));
-            animationView.setAnimation(R.raw.disconnected);
-            animationView.playAnimation();
         }
     }
 
@@ -217,75 +159,88 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_settings) {
-            startActivity(new Intent(this, SettingsActivity.class));
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
             appendLog("Opening settings");
-        } else if (id == R.id.action_logs) {
-            startActivity(new Intent(this, LogViewerActivity.class));
         }
         return super.onOptionsItemSelected(item);
     }
 
-    private void connect() {
-        tvStatus.setText("Status: Connecting...");
-        tvStatus.setTextColor(ContextCompat.getColor(this, R.color.connecting_orange));
-        animationView.setAnimation(R.raw.connecting);
-        animationView.playAnimation();
-        appendLog("Starting connection to: " + selectedAccount.getServer());
-
-        Intent intentPrepare = VpnService.prepare(this);
-        if (intentPrepare != null) {
-            startActivityForResult(intentPrepare, 0);
-            return;
-        }
-
-        try {
-            sshIntent.putExtra("ssh_account", selectedAccount);
-            startService(sshIntent);
-            appendLog("SSH service started");
-
-            vpnIntent.putExtra("socksPort", 1080);
-            startService(vpnIntent);
-            appendLog("VPN service started");
-            
-            StatusInfo.getInstance().setActive(true);
-            updateConnectionState();
-        } catch (Exception e) {
-            appendLog("Error: " + e.getMessage());
-            e.printStackTrace();
-            disconnect();
-        }
-    }
-
-    private void disconnect() {
-        tvStatus.setText("Status: Disconnecting...");
-        tvStatus.setTextColor(ContextCompat.getColor(this, R.color.disconnecting_orange));
-        animationView.setAnimation(R.raw.disconnecting);
-        animationView.playAnimation();
-        appendLog("Stopping services");
-
-        StatusInfo.getInstance().setActive(false);
-        
-        Thread vpnThread = SocksPersistent.getInstance().getVpnThread();
-        if (vpnThread != null) {
-            vpnThread.interrupt();
-            appendLog("VPN thread interrupted");
-        }
-
-        stopService(sshIntent);
-        stopService(vpnIntent);
-        appendLog("Services stopped");
-        
-        updateConnectionState();
-    }
-
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 0 && resultCode == RESULT_OK) {
-            connect();
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+        super.onActivityResult(requestCode, resultCode, resultData);
+    }
+
+    private void startVpn(String username, String password, String privateKey, String hostname, int port, String sniHost) {
+        if(!StatusInfo.getInstance().isActive()) {
+            tvStatus.setText("Status: Connecting...");
+            appendLog("Preparing VPN service");
+
+            Intent intentPrepare = VpnService.prepare(this);
+            if (intentPrepare != null) {
+                StatusInfo.getInstance().setActive(false);
+                appendLog("Toggled off due to missing VPN permission");
+                startActivityForResult(intentPrepare, 0);
+                return;
+            }
+
+            appendLog("Toggled on");
+            StatusInfo.getInstance().setActive(true);
+            tvStatus.setText("Status: Connecting...");
+
+            try {
+                appendLog("Setting up port forward");
+                sshIntent.putExtra("user", username);
+                sshIntent.putExtra("password", password);
+                if (privateKey != null && !privateKey.isEmpty()) {
+                    sshIntent.putExtra("privateKey", privateKey);
+                    appendLog("Using private key authentication");
+                } else {
+                    appendLog("Using password authentication");
+                }
+                sshIntent.putExtra("hostname", hostname);
+
+                if(port > 0 && port < 65536) {
+                    sshIntent.putExtra("port", String.valueOf(port));
+                    appendLog("Using custom port: " + port);
+                } else {
+                    sshIntent.putExtra("port", String.valueOf(22));
+                    appendLog("Using default SSH port: 22");
+                }
+
+                if (!sniHost.isEmpty()) {
+                    sshIntent.putExtra("sni", sniHost);
+                    appendLog("Using SNI: " + sniHost);
+                }
+
+                startService(sshIntent);
+                appendLog("SSH service started");
+
+                vpnIntent.putExtra("socksPort", 1080);
+                startService(vpnIntent);
+                appendLog("VPN service started");
+                StatusInfo.getInstance().setActive(true);
+                tvStatus.setText("Status: Connected");
+            } catch (Exception e) {
+                StatusInfo.getInstance().setActive(false);
+                tvStatus.setText("Status: Error - " + e.getMessage());
+                appendLog("Error: " + e.getMessage());
+            }
         } else {
-            appendLog("VPN permission denied");
-            updateConnectionState();
+            appendLog("Toggled off");
+            StatusInfo.getInstance().setActive(false);
+            tvStatus.setText("Status: Disconnecting...");
+
+            Thread vpnThread = SocksPersistent.getInstance().getVpnThread();
+            if(vpnThread != null) {
+                vpnThread.interrupt();
+                appendLog("VPN thread interrupted");
+            }
+
+            sshIntent = StatusInfo.getInstance().getSshIntent();
+            stopService(sshIntent);
+            appendLog("Services stopped");
+            tvStatus.setText("Status: Disconnected");
         }
     }
 }
