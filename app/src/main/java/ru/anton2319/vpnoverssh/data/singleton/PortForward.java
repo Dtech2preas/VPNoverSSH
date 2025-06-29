9f4981cc179a4c1e1923be61f6b3b439653567f8
Dtech2preas <lefa4082@gmail.com>
2025-06-29 21:26:20 +0200

    Update PortForward.java

--- app/src/main/java/ru/anton2319/vpnoverssh/data/singleton/PortForward.java
+++ app/src/main/java/ru/anton2319/vpnoverssh/data/singleton/PortForward.java
@@ -4,48 +4,59 @@ import com.trilead.ssh2.Connection;
 import com.trilead.ssh2.DynamicPortForwarder;
 
 public class PortForward {
-    private static volatile PortForward instance = null;
-    private static final Object lock = new Object();
-
-    private Thread sshThread;
+    private static PortForward instance = null;
     private Connection conn;
     private DynamicPortForwarder forwarder;
-
+    private Thread sshThread;
+    private boolean proxyReady = false;
 
     private PortForward() {}
 
-    public static PortForward getInstance() {
+    public static synchronized PortForward getInstance() {
         if (instance == null) {
-            synchronized (lock) {
-                if (instance == null) {
-                    instance = new PortForward();
-                }
-            }
+            instance = new PortForward();
         }
         return instance;
     }
 
-    public void setSshThread(Thread sshThread) {
-        this.sshThread = sshThread;
+    // Add the required methods
+    public synchronized boolean isProxyReady() {
+        return proxyReady;
     }
 
-    public Thread getSshThread() {
-        return sshThread;
+    public synchronized void setProxyReady(boolean ready) {
+        proxyReady = ready;
+    }
+
+    // Existing methods below...
+    public Connection getConn() {
+        return conn;
     }
 
     public void setConn(Connection conn) {
         this.conn = conn;
     }
 
-    public Connection getConn() {
-        return conn;
+    public DynamicPortForwarder getForwarder() {
+        return forwarder;
     }
 
     public void setForwarder(DynamicPortForwarder forwarder) {
         this.forwarder = forwarder;
     }
 
-    public DynamicPortForwarder getForwarder() {
-        return forwarder;
+    public Thread getSshThread() {
+        return sshThread;
+    }
+
+    public void setSshThread(Thread sshThread) {
+        this.sshThread = sshThread;
+    }
+
+    public void cleanup() {
+        this.conn = null;
+        this.forwarder = null;
+        this.sshThread = null;
+        this.proxyReady = false;
     }
 }
